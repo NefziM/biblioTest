839a7de916ba5b81a501c59847fe9231216b360e
NefziM <nefzimalek2002@gmail.com>
2024-12-10 04:54:12 +0100

    version 2

--- src/test/java/Steps/ListeSteps.java
+++ src/test/java/Steps/ListeSteps.java
@@ -5,22 +5,25 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ListeSteps {
-	@Given("je suis un utilisateur connecté")
-	public void je_suis_un_utilisateur_connecté() {
-	    System.out.println("Inside Step - un utilisateur connecté ");
-	}
+  
+  // Etape Given : utilisateur connecté
+  @Given("je suis un utilisateur connecté")
+  public void je_suis_un_utilisateur_connecté() {
+      // Action simulée pour indiquer que l'utilisateur est connecté
+      System.out.println("Inside Step - un utilisateur connecté ");
+  }
 
-	@When("je consulte la liste des livres")
-	public void je_consulte_la_liste_des_livres() {
-		System.out.println("Inside Step - un utilisateur consulte la liste des livres ");
-	}
+  // Etape When : consultation de la liste des livres
+  @When("je consulte la liste des livres")
+  public void je_consulte_la_liste_des_livres() {
+      // Action simulée pour indiquer que l'utilisateur consulte les livres
+      System.out.println("Inside Step - un utilisateur consulte la liste des livres ");
+  }
 
-	@Then("je devrais voir tous les livres disponibles à la bibliothèque")
-	public void je_devrais_voir_tous_les_livres_disponibles_à_la_bibliothèque() {
-		System.out.println("Inside Step - un utilisateur voir tous les livres disponibles à la bibliothèque ");
-	}
-
-
-
-
+  // Etape Then : voir les livres disponibles
+  @Then("je devrais voir tous les livres disponibles à la bibliothèque")
+  public void je_devrais_voir_tous_les_livres_disponibles_à_la_bibliothèque() {
+      // Action simulée pour indiquer que l'utilisateur voit les livres disponibles
+      System.out.println("Inside Step - un utilisateur voir tous les livres disponibles à la bibliothèque ");
+  }
 }
--- src/test/resources/Features/listebiblio.feature
+++ src/test/resources/Features/listebiblio.feature
@@ -1,11 +1,7 @@
-
 @listlivre
-Feature: Consultation des livres
-
+Feature: Gestion des livres
   @dispolivre
-  Scenario: Affichage de la liste des livres disponibles
+  Scenario: Affichage des livres disponibles
     Given je suis un utilisateur connecté
     When je consulte la liste des livres
     Then je devrais voir tous les livres disponibles à la bibliothèque
-
-
